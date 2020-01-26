4959e505012984d36b3ab8f7661e9307b163f0f6
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2020-01-26 19:56:59 +0100

    begin building addBookToLibrary() functionality

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
-let myLibrary = [
-  [''],
-];
+let book;
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.noOfPages = pages;
   this.readOrNot = read;
-
-  let readStatus;
-
-  if (read === true) {
-    readStatus = 'already read';
-  } else {
-    readStatus = 'not read yet';
-  }
-
-  this.info = function () {
-    return title + ' by ' + author + ', ' + pages + ' pages, ' + ', ' + readStatus;
-  };
-
 }
+
+let hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 310, true);
+let haus = new Book('Haus der Geister', 'John Boyne', 333, true);
+
+let myLibrary = [hobbit, haus];
 
 function addBookToLibrary() {
+  let bookTitle = document.getElementById('book-title');
+  let bookAuthor = document.getElementById('book-author');
+  let pagesNumber = document.getElementById('no-of-pages');
+  let read = document.getElementById('read');
+  let notRead = document.getElementById('not-read');
+  let submit = document.getElementById('submit');
 
+  let ifRead = document.getElementsByName('readStatus');
+
+  submit.addEventListener('click', () => {
+    if (ifRead[0].checked) {
+      ifRead = read.value;
+    } else if (ifRead[1].checked) {
+      ifRead = notRead.value;
+    }
+
+    book = new Book(bookTitle.value, bookAuthor.value, pagesNumber.value, ifRead);
+    myLibrary.push(book);
+  });
 }
 
-let hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 456, true);
+addBookToLibrary();
 
-document.getElementById('demo').innerHTML = hobbit.info();
-
+// FORM CREATION-------------------------------------------------------------
 let formOpener = document.getElementById('new-book');
 let form = document.getElementById('form');
 let cancel = document.getElementById('cancel');
@@ -40,4 +47,4 @@ formOpener.addEventListener('click', () => {
 
 cancel.addEventListener('click', () => {
   form.style.display = 'none';
-})
+});
