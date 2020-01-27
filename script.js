4f9627e5efb2cd689216836eb8ad3b83bcb81ad3
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2020-01-27 01:09:26 +0100

    create render() with ability to render first value of array so far

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 let book;
 
+let bookTitle = document.getElementById('book-title');
+let bookAuthor = document.getElementById('book-author');
+let pagesNumber = document.getElementById('no-of-pages');
+let read = document.getElementById('read');
+let notRead = document.getElementById('not-read');
+let submit = document.getElementById('submit');
+let ifRead = document.getElementsByName('readStatus');
+let library = document.getElementById('library-container');
+
+let bookshelf = document.createElement('div');
+let description = document.createElement('p');
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -13,15 +25,6 @@ let haus = new Book('Haus der Geister', 'John Boyne', 333, true);
 let myLibrary = [hobbit, haus];
 
 function addBookToLibrary() {
-  let bookTitle = document.getElementById('book-title');
-  let bookAuthor = document.getElementById('book-author');
-  let pagesNumber = document.getElementById('no-of-pages');
-  let read = document.getElementById('read');
-  let notRead = document.getElementById('not-read');
-  let submit = document.getElementById('submit');
-
-  let ifRead = document.getElementsByName('readStatus');
-
   submit.addEventListener('click', () => {
     if (ifRead[0].checked) {
       ifRead = read.value;
@@ -31,7 +34,22 @@ function addBookToLibrary() {
 
     book = new Book(bookTitle.value, bookAuthor.value, pagesNumber.value, ifRead);
     myLibrary.push(book);
+
+    render();
   });
+}
+
+function render() {
+  let librLength = myLibrary.length;
+
+  for (let i = 0; i < librLength; i++) {
+    let valueArray = Object.values(myLibrary[i]);
+    bookshelf.setAttribute('class', 'book-item');
+    library.appendChild(bookshelf);
+    bookshelf.appendChild(description);
+    description.textContent = 'Book Title: ' + valueArray[0];
+    description.setAttribute('class', 'book-info');
+  }
 }
 
 addBookToLibrary();
