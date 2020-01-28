2f0eb45155e366d10eef6e71b3328e244e649132
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2020-01-28 16:48:29 +0100

    fix bug with read status

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 let book;
 
+let formOpener = document.getElementById('new-book');
+let form = document.getElementById('form');
+let cancel = document.getElementById('cancel');
 let bookTitle = document.getElementById('book-title');
 let bookAuthor = document.getElementById('book-author');
 let pagesNumber = document.getElementById('no-of-pages');
 let read = document.getElementById('read');
 let notRead = document.getElementById('not-read');
 let submit = document.getElementById('submit');
+let textareaTitle = document.getElementById('book-title');
+let textareaAuthor = document.getElementById('book-author');
+let textareaPages = document.getElementById('no-of-pages');
 let ifRead = document.getElementsByName('readStatus');
 let library = document.getElementById('library-container');
 
@@ -14,37 +20,30 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.noOfPages = pages;
   this.readOrNot = read;
+
+  if (read === true) {
+    this.readOrNot = 'already read';
+  } else {
+    this.readOrNot = 'not read yet';
+  }
 }
 
-let hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 310, 'already read');
-let haus = new Book('This House is Haunted', 'John Boyne', 333, 'already read');
+let hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 310, true);
+let house = new Book('This House is Haunted', 'John Boyne', 333, true);
+let hard = new Book('Hard Sayings', 'Trent Horn', 276, false);
 
-let myLibrary = [hobbit, haus];
+let myLibrary = [hobbit, house, hard];
 
-myrender();
+render();
+addBookToLibrary();
 
-function addBookToLibrary() {
-  submit.addEventListener('click', () => {
-    form.style.display = 'none';
-    if (ifRead[0].checked) {
-      ifRead = read.value;
-    } else if (ifRead[1].checked) {
-      ifRead = notRead.value;
-    }
-
-    book = new Book(bookTitle.value, bookAuthor.value, pagesNumber.value, ifRead);
-    myLibrary.push(book);
-    library.innerHTML = '';
-    myrender();
-  });
-}
-
-function myrender() {
+function render() {
   let librLength = myLibrary.length;
 
   for (let i = 0; i < librLength; i++) {
     let bookshelf = document.createElement('div');
     let currentBook = myLibrary[i];
+
     [
       { 'Book Title: ': currentBook.title },
       { 'Book Author: ': currentBook.author },
@@ -55,26 +54,53 @@ function myrender() {
 
       let prefix = Object.keys(trait)[0];
       let suffix = Object.values(trait)[0];
+
       p.textContent = prefix + suffix;
       bookshelf.appendChild(p);
     });
 
-    // let valueArray = Object.values(myLibrary[i]);
-    bookshelf.setAttribute('class', 'book-item');
+    bookshelf.className += 'book-item ';
+    bookshelf.className += `book-item${i + 1}`;
     library.appendChild(bookshelf);
 
-    // bookshelf.appendChild(description);
-    // description.textContent = 'Book Title: ' + myLibrary[i].title;
-    // description.setAttribute('class', 'book-info');
+    let removeOption = document.createElement('button');
+    removeOption.innerHTML = 'remove';
+    removeOption.className += 'delete-button ';
+    removeOption.className += `delete-button${i + 1}`;
+
+    bookshelf.appendChild(removeOption);
+
+    let deleteCandidate = document.querySelector(`div[class="book-item${i + 1}"]`);
+    let buttonOfDeleteCandidate = document.querySelector(`button[class="delete-button${i + 1}"]`);
+
+    // buttonOfDeleteCandidate.addEventListener('click', () => {
+    //   library.removeChild(deleteCandidate);
+    // });
   }
 }
 
-addBookToLibrary();
+function addBookToLibrary() {
+  submit.addEventListener('click', () => {
+    form.style.display = 'none';
+    let readStatus;
+    book = new Book(bookTitle.value, bookAuthor.value, pagesNumber.value, readStatus);
+
+    if (ifRead[0].checked) {
+      book.readOrNot = 'already read';
+    } else if (ifRead[1].checked) {
+      book.readOrNot = 'not read yet';
+    } else {
+      book.readOrNot = 'not determined';
+    }
+
+    myLibrary.push(book);
+    library.innerHTML = '';
+    render();
+    clearForm();
+  });
+}
 
 // FORM CREATION-------------------------------------------------------------
-let formOpener = document.getElementById('new-book');
-let form = document.getElementById('form');
-let cancel = document.getElementById('cancel');
 
 formOpener.addEventListener('click', () => {
   form.style.display = 'block';
@@ -82,4 +108,13 @@ formOpener.addEventListener('click', () => {
 
 cancel.addEventListener('click', () => {
   form.style.display = 'none';
+  clearForm();
 });
+
+function clearForm() {
+  textareaTitle.value = '';
+  textareaAuthor.value = '';
+  textareaPages.value = '';
+  read.checked = false;
+  notRead.checked = false;
+}
